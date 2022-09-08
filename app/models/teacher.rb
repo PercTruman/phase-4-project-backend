9f331b370eb2eec9edd5a84de3461df932e515a3
PercTruman <drshawnhart@gmail.com>
2022-09-07 20:32:31 -0500

    added email format validation

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -5,6 +5,7 @@ class Teacher < ApplicationRecord
 
     validates :first_name, presence: true
     validates :last_name, presence: true
-    validates :email, presence: true, uniqueness: :true
+    validates :email, presence: true, uniqueness: :true, format: {with: /^(.+)@(.+)$/, message: "Email invalid"  },
+            length: { minimum: 4, maximum: 254}
     validates :password, presence: true
 end
